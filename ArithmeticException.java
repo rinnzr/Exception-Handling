d87f865dcf9fcd907d10765ccc08e593db217025
rinnzzr <101549636+rinnzzr@users.noreply.github.com>
2022-05-17 10:02:11 +0700

    Add files via upload

---
+++ ArithmeticException.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception.handling;
+import java.util.Scanner;
+
+/**
+ *
+ * @author orinz
+ */
+public class ArithmeticException {
+    public static void main(String[] args) { 
+        //int a=100;
+    //      System.out.println(&quot;Enter the value for b&quot;);
+        Scanner console = new Scanner(System.in);
+    // int b = console.nextInt();
+
+    System.out.println("Enter the value for c");
+    
+    // Exception Handling
+        try{
+    int c = console.nextInt();
+        int res=c/0;
+        // 10/(b-c);    
+        System.out.println("The result is "+res);
+        }
+        catch (Exception exception_message){
+            System.out.println("adalah = " +exception_message);
+        }
+    
+    }}
+        
---
+++ TestThrow.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception.handling;
+
+/**
+ *
+ * @author orinz
+ */
+    class AgeInvalid extends Exception{
+    AgeInvalid(String s){
+        super(s);
+    }
+}
+
+  public class TestThrow {
+    static void validate(int age) throws AgeInvalid {
+        if (age < 18)
+            throw new AgeInvalid("not valid ");
+        else
+            System.out.println("welcome to vote");
+    }
+
+    public static void main(String args[]) {
+        try {
+            validate(13);
+        } catch (Exception i) {
+            System.out.println("Terkana Exception: " + i);
+        }
+        System.out.println("rest of the code...");
+    }
+}
